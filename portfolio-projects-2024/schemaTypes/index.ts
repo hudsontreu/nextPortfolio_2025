22666519c04d9538522798bfaa97909bbd759019
Hudson Treu <hudsontreu55@gmail.com>
2025-02-14 22:38:43 -0500

    Remove old project directories after restructuring

--- portfolio-projects-2024/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import blockContent from './blockContent'
-import projects from './projectsType'
-import experiments from './experimentsType'
-
-export const schemaTypes = [projects, experiments, blockContent]
